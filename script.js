5cd33e5c33ac8c74e9a9d7ad806780a7bfcb7a55
CrueltySquad <152569790+CrueltySquad@users.noreply.github.com>
2024-02-21 17:56:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const WORDS = [
-  "APPLE",
-  "BANANA",
-  "BLACKB",
-  "BLUEB",
-  "CHERRY",
-  "CLEMENTINE",
-  "COCONUT",
-  "DURIAN",
-  "ELDERB",
-  "FIG",
-  "GRAPE",
-  "GUAVA",
-  "JACKFRUIT",
-  "KIWI",
+import { WORDS } from "./words.js";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(rightGuessString)
